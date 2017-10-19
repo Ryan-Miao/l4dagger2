e4e00552b9420c06dff184364a95cee84f65ca87
Ryan Miao <woshimrf@126.com>
2017-10-19 09:01:29 +0800

    lio add 3

--- src/main/java/com/test/l4dagger2/hello/HiDagger.java
+++ src/main/java/com/test/l4dagger2/hello/HiDagger.java
@@ -4,6 +4,8 @@ package com.test.l4dagger2.hello;
  * lio create this.
  *
  * The hello world of dagger2.
+ *
+ * reference: https://google.github.io/dagger/users-guide
  */
 public class HiDagger {
 
